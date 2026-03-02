66820a7e303a89978cdda203967c4248cc137c91
LadyBeGood <infinixutkarsh@gmail.com>
2026-03-02 18:20:13 +0530

    Embedded SVGs directly inside HTML

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,11 @@
 
 
 
-<div class="fixed left-0 right-0 top-0 flex justify-between mx-5 my-5">
-    <div class="p-2.5 border-2 rounded-full border-luxury-white/10">
-        <img src="./music.svg" alt="music" height="22px" width="22px">
+<div class="fixed left-0 right-0 top-0 flex justify-between mx-5 my-5 text-luxury-white">
+    <div class="p-2.5 border-2 rounded-full border-luxury-white/10 bg-blackout">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M127-167q-47-47-47-113t47-113q47-47 113-47 23 0 42.5 5.5T320-418v-308q0-15 9.5-26.5T353-766l400-66q18-3 32.5 8.5T800-793v433q0 66-47 113t-113 47q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T720-498v-165l-320 63v320q0 66-47 113t-113 47q-66 0-113-47Z"/></svg>
     </div>
-    <div class="p-2.5 border-2 rounded-full border-luxury-white/10">
-        <img src="./settings.svg" alt="settings" height="22px" width="22px">
+    <div class="p-2.5 border-2 rounded-full border-luxury-white/10 bg-blackout">
+       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M433-80q-27 0-46.5-18T363-142l-9-66q-13-5-24.5-12T307-235l-62 26q-25 11-50 2t-39-32l-47-82q-14-23-8-49t27-43l53-40q-1-7-1-13.5v-27q0-6.5 1-13.5l-53-40q-21-17-27-43t8-49l47-82q14-23 39-32t50 2l62 26q11-8 23-15t24-12l9-66q4-26 23.5-44t46.5-18h94q27 0 46.5 18t23.5 44l9 66q13 5 24.5 12t22.5 15l62-26q25-11 50-2t39 32l47 82q14 23 8 49t-27 43l-53 40q1 7 1 13.5v27q0 6.5-2 13.5l53 40q21 17 27 43t-8 49l-48 82q-14 23-39 32t-50-2l-60-26q-11 8-23 15t-24 12l-9 66q-4 26-23.5 44T527-80h-94Zm49-260q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Z"/></svg>
     </div>
 </div>
--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-
 </script>
 
-<div 
-    class="text-luxury-white w-48 h-48 rounded-full p-2.5 relative font-[Manrope] tabular-nums" 
-    style="background: conic-gradient(var(--color-dravit-grey) 0% 60%, var(--color-luxury-white) 60% 100%);"
->
-    <!-- Small circle -->
-    <div class="h-2.5 w-2.5 absolute bg-luxury-white rounded-full left-1/2 top-0 -translate-x-1/2"></div>
-    <!-- rotating circle -->
-    <div class="absolute h-full  left-1/2 top-0 -translate-x-1/2" style="rotate: calc(60deg * 3.6);">
-        <div class="h-5 w-5 bg-luxury-white rounded-full -translate-y-1/4"></div>
+<div class="flex items-center flex-col gap-4">
+    <div
+        class="text-luxury-white w-48 h-48 rounded-full p-2.5 relative font-[Manrope] tabular-nums"
+        style="background: conic-gradient(var(--color-dravit-grey) 0% 60%, var(--color-luxury-white) 60% 100%);"
+    >
+        <!-- Small circle -->
+        <div class="h-2.5 w-2.5 absolute bg-luxury-white rounded-full left-1/2 top-0 -translate-x-1/2"></div>
+        <!-- rotating circle -->
+        <div
+            class="absolute h-full left-1/2 top-0 -translate-x-1/2"
+            style="rotate: calc(60deg * 3.6);"
+        >
+            <div class="h-5 w-5 bg-luxury-white rounded-full -translate-y-1/4"></div>
+        </div>
+        <div class="h-full w-full rounded-full bg-blackout grid place-items-center text-4xl">
+            <p>01:30</p>
+        </div>
     </div>
-    <div class="h-full w-full rounded-full bg-blackout grid place-items-center text-4xl">
-        <p>
-            01:30
-        </p>
+
+    <div class="bg-luxury-white rounded-full p-3 translate-y-6">
+        <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="36px" fill="currentColor"><path d="M320-273v-414q0-17 12-28.5t28-11.5q5 0 10.5 1.5T381-721l326 207q9 6 13.5 15t4.5 19q0 10-4.5 19T707-446L381-239q-5 3-10.5 4.5T360-233q-16 0-28-11.5T320-273Z"/></svg>
     </div>
 </div>
-
