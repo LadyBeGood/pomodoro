1e610fa0330c2dfa796f77992d3316243c00d714
LadyBeGood <infinixutkarsh@gmail.com>
2026-03-15 12:19:44 +0530

    Configured vite properly for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+        outDir: "docs"
+    },
+    base: "./",
     plugins: [
         svelte(),
         tailwindcss()
